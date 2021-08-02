e6d9d8729633227880b4fee3a494f7dae4e9f399
Eugen Kakareka <evv.kakareka@gmail.com>
2021-08-02 17:28:54 +0300

    task2 add

--- index.js
+++ index.js
@@ -27,9 +27,33 @@ objToArray()
 
 //task2
 
-function getArray() {
-    const amount = 9;
+const amount = 12
 
+function getArray(amount) {
+
+    if (amount % 3 == 0) {
+        const subarraySize = 3
+        let array = []
+        let slicedArray = []
+
+        for (let i = 1; i <= amount; i++) {
+            array.push(i)
+        }
+
+        for (let i = 0; i < array.length; i += subarraySize) {
+            slicedArray.push(array.slice(i, i + subarraySize));
+        }
+        console.log(slicedArray)
+    } else {
+        console.log('Максимальное значение должно делиться на 3 нацело.')
+    }
 }
 
-getArray()
+getArray(amount)
+
+//task3
+
+const namesOfDays = {
+    ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+}
