9c80a8049f8270e7648a50302c0c0c0114c1783b
Eugen Kakareka <evv.kakareka@gmail.com>
2021-08-04 23:45:51 +0300

    task 4 -5  add

--- index.js
+++ index.js
@@ -19,5 +19,23 @@ let createEmployesFromArr = (arr) => {
     arr.forEach((employee) => employers.push(new Emploee(employee)))
     return employers;
 };
-
 const employeeConstructArr = createEmployesFromArr(employeeArr)
+
+const getFullNamesFromArr = (arr) => {
+
+    return arr.map(item => item.getFullName())
+}
+
+const fullNameList = getFullNamesFromArr(employeeConstructArr)
+
+const getMiddleSalary = (arr) => {
+
+    let salarySum = 0
+
+    for (let i = 0; i < arr.length; i++) {
+        salarySum += arr[i].salary
+    }
+    return Math.round(salarySum / arr.length)
+}
+
+const middleSalary = getMiddleSalary(employeeConstructArr)
