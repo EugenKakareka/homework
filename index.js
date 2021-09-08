69e98ab46b25be3dd351f39eea172bd8e4f4ffc2
Eugen Kakareka <evv.kakareka@gmail.com>
2021-09-08 23:41:32 +0300

    task 2 add

--- index.js
+++ index.js
@@ -19,3 +19,31 @@ const game = new Promise((resolve, reject) => {
         }
     })
     .catch(() => { console.log('Exit') })
+
+//task2
+
+function goToShop() {
+    let number = Math.floor(Math.random() * 6) + 1
+    return Promise.resolve(number)
+}
+
+function makeDinner() {
+    return Promise.resolve(
+        setTimeout(() => { console.log('Bon Appetit') }, 3000)
+    )
+}
+
+class ProductError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'ProductError';
+    }
+}
+goToShop().then((number) => {
+    if (number < 4) {
+        return Promise.reject('Too low products')
+    } else makeDinner()
+}).catch((message) => {
+
+    throw new ProductError(message)
+})
