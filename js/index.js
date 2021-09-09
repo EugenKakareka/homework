7613e2eaa0074e0dd255c8bce714fbef816fba72
Eugen Kakareka <evv.kakareka@gmail.com>
2021-09-09 15:41:00 +0300

    task 3

--- js/index.js
+++ js/index.js
@@ -49,3 +49,61 @@ goToShop().then((number) => {
 })
 
 //task 3
+
+const getData = () => fetch('https://rickandmortyapi.com/api/character')
+    .then((response) => response.json())
+    .then((data) => {
+        render(data.results)
+    });
+
+
+const getFilterData = (param) => fetch('https://rickandmortyapi.com/api/character' + `/${param}`)
+    .then((response) => response.json())
+    .then((data) => {
+        render(data.results)
+    });
+
+const getDataById = (arrID) => fetch('https://rickandmortyapi.com/api/character' + `/${arrID}`)
+    .then((response) => response.json())
+    .then((data) => {
+        render(data.results)
+    });
+
+
+document.querySelector('.form-container').addEventListener('click', (event) => {
+    let path = event.target.id
+    if (path === 'male' || path === 'female') {
+        let param = '?gender=' + path
+        getFilterData(param)
+    }
+    if (path === 'alive' || path === 'dead') {
+        let param = '?status=' + path
+        getFilterData(param)
+    }
+})
+
+let template = (item) => { `<div class="card">
+<div class="card-info">
+    <div class="title">
+        <h1>${item.name}</h1>
+        <div class="status">
+            <div class="live-status"></div>
+            <p>${item.status}</p>
+        </div>
+    </div>
+    <div class="content">
+        <p>${item.location.name}</p>
+    </div>
+</div>
+<div class="card-image">
+    <img src="${item.image}" alt="Img">
+</div>
+</div>` }
+
+
+function render(items) {
+    const html = items.map(item => template(item)).join('')
+    document.body.querySelector('.container').innerHTML = html
+}
+
+getData()
